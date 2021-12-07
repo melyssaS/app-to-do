c94254a0cd06933be1f2929b93b9d34830feaa7c
melyssaS <melyssasolanod@gmail.com>
2021-12-06 22:09:37 -0500

    fix command.sh

--- command.sh
+++ command.sh
@@ -1,8 +1,12 @@
 cd backend
 npm init
+npm install
 docker-compose up -d postgres
 docker-compose up -d pgadmin
 cd ..
 cd frontend 
 npm init
+npm install
+npm run serve & (cd ../backend && npm run start:dev)
+
 
